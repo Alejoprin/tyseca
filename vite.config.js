3836a8c7a84d769cdf5b10ea33f86002eaa89efe
Alejandro Calderon <bass.prin@gmail.com>
2026-04-06 23:59:16 +0200

    Quitar formulario, agregar redes sociales en la seccion de contactenos, terminar las meta tag con el dominio real

--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         useShortDoctype: true,
         minifyCSS: true,
         minifyJS: true,
+        ignoreCustomComments: [/^!/],
       },
     }),
     viteImagemin({
